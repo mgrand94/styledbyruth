5127c83db12b17b9b8599064478e767d77b20887
Your Name <Marcosemail2015@gmail.com>
2022-09-09 11:07:08 -0600

    Styledbyruthweb

---
+++ index.js
@@ -0,0 +1,20 @@
+
+//navigation to other pages
+function navBarButton1 () {
+    window.location='index.html';
+    }
+
+function navBarButton2 () {
+    window.location='secondarypage.html';
+    }
+
+function navBarButton3 () {
+    window.location='secondarypage.html';
+    }
+
+function navBarButton4 () {
+    window.location='secondarypage.html';
+    }
+    
+
+  
